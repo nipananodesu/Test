d1ef974f9054ed60d4efa7da32bc7cc1b3ec4b44
nipananodesu <nipananodesu@yahoo.com>
2017-02-09 18:02:09 +0800

    adt

--- CodeLearning/Test/Test/main.cpp
+++ CodeLearning/Test/Test/main.cpp
@@ -1,16 +1,34 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <numeric>
+#include <time.h>
 
 using namespace std;
 
+clock_t start, stop;
+double duration;
+
+class Sort 
+{
+public:
+	void sort(vector<int> &vi);
+	bool less(int &v, int &w);
+	void exch(int &i, int &j);
+	void show(int &a);
+	bool isSorted(vector<int> &vi);
+	void input(vector<int> &vi);
+};
 
 int main()
 {
-	string word1;
-	string word2;
-	while (getline(cin,word1))
-		while(getline(cin,word2))
-		cout << word1+word2 << endl;
+	start = clock();
+
+
+
+	stop = clock();
+	duration = ((double)(stop - start)) / CLK_TCK;
+	cout << duration << endl;
 
 	return 0;
 }
